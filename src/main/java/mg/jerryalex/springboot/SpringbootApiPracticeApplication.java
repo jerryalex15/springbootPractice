da586f100a41d6adcfecb80089af6d8e12eb521f
jerryalex15 <jerryRazafindraibe@gmail.com>
2022-03-21 20:50:49 +0300

    initial project

---
+++ src/main/java/mg/jerryalex/springboot/SpringbootApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package mg.jerryalex.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/jerryalex/springboot/SpringbootApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.jerryalex.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
